fb892597980b11c7bb4e753b7c631540fe038c99
soyeon <soyeon@naver.com>
2024-07-16 13:06:19 +0900

    Java20716

---
+++ src/day04/practice/nameless/Person.java
@@ -0,0 +1,10 @@
+package day04.practice.nameless;
+
+public abstract class Person {
+	String name;
+	int age;
+	
+	public abstract void showInfo();
+	
+	
+}
---
+++ src/day04/task/abstractTest/MovieStudent.java
@@ -0,0 +1,9 @@
+package day04.task.abstractTest;
+
+public class MovieStudent extends Student {
+	@Override
+	public void hobby() {
+		System.out.println("영화를 본다.");
+	}
+	
+}
