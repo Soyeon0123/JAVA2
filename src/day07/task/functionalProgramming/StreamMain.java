1dd6f64afde02bd429b66db5b19602164a45a626
soyeon <soyeon@naver.com>
2024-07-19 13:57:45 +0900

    java2 final

---
+++ src/day07/task/functionalProgramming/StreamMain.java
@@ -0,0 +1,23 @@
+package day07.task.functionalProgramming;
+
+import java.util.stream.IntStream;
+
+public class StreamMain {
+
+	public static void main(String[] args) {
+		// 1부터 100까지의 수 중 짝수만 출력하는 프로그램 작성
+		IntStream streamInt = IntStream.rangeClosed(0, 100);
+		streamInt.filter(e -> e % 2 ==0).forEach(System.out::println);
+		
+		// 1부터 100까지의 수 중 홀수만 출력하는데
+		// 3의 배수인 홀수만 출력하는 프로그램 작성
+		streamInt.filter(e -> e % 2 ==1).filter(e -> e % 3 == 0).forEach(System.out::println);
+		
+		
+		
+		
+		
+		
+	}
+
+}
